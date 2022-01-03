bace0338b8f53e536f1c09fd1d24e4a2d22ed29a
Çlirim Krasniqi <qliraa19@gmail.com>
2022-01-03 19:11:24 +0100

    Persistance storage

--- src/store/store.js
+++ src/store/store.js
@@ -4,9 +4,13 @@ import Vuex from "vuex";
 import movies from "./modules/movies";
 import users from "./modules/users";
 
+import createPersistedState from "vuex-persistedstate";
+import * as Cookies from "js-cookie";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  plugins: [createPersistedState()],
   modules: {
     movies,
     users,
